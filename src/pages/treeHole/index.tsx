b19a8c925340a02ff34baa516f856a0f1652dc28
chenheng <chenheng@chenhengdeMacBook-Pro.local>
2020-11-12 13:54:00 +0800

    1

--- src/pages/treeHole/index.tsx
+++ src/pages/treeHole/index.tsx
@@ -11,15 +11,24 @@ class TreeHole extends Component<any, any>{
                 onChange={(key) => {console.log(key)}}
             >
                 {interact.treeHoles.map((it: any, i: number) => (
-                    <Collapse.Panel header={`${it.type === 1 ? "学习小树洞" : it.type === 2 ? "心灵小树洞" : "家长意见"} （
-                    ${it.anonymous ? "匿名" : it.issuener.name}）: ${it.issue}`} key={i}>
+                    <Collapse.Panel
+                        key={it.id}
+                        header={<div style={{position: 'relative'}}>
+                            <div style={{fontSize: 12}}>
+                                {`${it.type === 1 ? "学习小树洞" : it.type === 2 ? "心灵小树洞" : "家长意见"}`}
+                                {` ( ${it.anonymous ? "匿名" : it.issuener.name} )`}
+                                {` ( ${it.created_at} ) : `}
+                            </div>
+                            <div style={{fontSize: 14}}>{`${it.issue}`}</div>
+                        </div>}
+                    >
                         {it.comments.map((item: any, index: any) => (
-                            <div key={index}>
-                                <div>{item.name} : {item.content}</div>
-                                <div style={{textAlign: "right"}}>--{item.created_at}</div>
+                            <div key={index} style={{paddingLeft: 30}}>
+                                <div style={{fontSize: 13}}>{item.name} : {item.content}</div>
+                                <div style={{textAlign: "right", fontSize: 12}}>— {item.created_at}</div>
                             </div>
                         ))}
-                        <div>
+                        <div style={{paddingLeft: 30}}>
                             <Input
                                 style={{width: 300, marginRight: 20}}
                                 value={it.comment}
